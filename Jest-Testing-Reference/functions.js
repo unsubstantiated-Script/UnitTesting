d8d0f54bdd12c0cb91489b33ea7c1d4423b523e1
unsubstantiated-Script <jusfulton@gmail.com>
2021-10-06 20:13:16 -0700

    Jest ref done

--- Jest-Testing-Reference/functions.js
+++ Jest-Testing-Reference/functions.js
@@ -1,5 +1,37 @@
 const axios = require("axios");
 
+//Lifecycle methods that happen before and after each function in the list
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => {
+// 	console.log("db initalized");
+// };
+// const closeDatabase = () => {
+// 	console.log("db closed");
+// };
+
+const nameCheck = () => {
+	console.log("Checking name");
+};
+
+describe("Checking names", () => {
+	beforeEach(() => nameCheck());
+
+	test("user is Jeff", () => {
+		const user = "Jeff";
+		expect(user).toBe("Jeff");
+	});
+
+	test("user is Karen", () => {
+		const user = "Karen";
+		expect(user).toBe("Karen");
+	});
+});
+
 const functions = {
 	add: (a, b) => a + b,
 	isNull: () => null,
